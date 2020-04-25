b505ce72f4ee13ddbbd7e8400f738c7ee4d6eef1
Ahad Islam <ahad.islam624@gmail.com>
2020-04-25 12:32:45 -0400

    animating simple shapes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -182,7 +182,7 @@ struct ColorCircleContentView: View {
     }
 }
 
-struct ContentView: View {
+struct BlendContentView: View {
     @State private var amount: CGFloat = 0.0
     
     var body: some View {
@@ -213,6 +213,44 @@ struct ContentView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.black)
         .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLines([
+            CGPoint(x: insetAmount, y: rect.minY),
+            CGPoint(x: rect.maxX - insetAmount, y: rect.minY),
+            CGPoint(x: rect.maxX, y: rect.maxY),
+            CGPoint(x: 0, y: rect.maxY)])
+        
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var insetAmount: CGFloat = 50
+    
+    
+    
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                self.insetAmount = CGFloat.random(in: 10...90)
+                }
+        }
     }
 }
 
