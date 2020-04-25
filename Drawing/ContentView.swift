aa5b2aaef32ca5be002995337f29cb5425cd9b38
Ahad Islam <ahad.islam624@gmail.com>
2020-04-25 11:32:28 -0400

    conforming arc to insettabnle shape to make it behave moralize a circle

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -34,21 +34,6 @@ struct Triangle: Shape {
     }
 }
 
-struct Arc: Shape {
-    var startAngle: Angle
-    var endAngle: Angle
-    var clockwise: Bool
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjustment
-        let modifiedEnd = endAngle - rotationAdjustment
-        
-        var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: clockwise)
-        return path
-    }
-}
 
 struct TriangleShapeView: View {
     var body: some View {
@@ -59,11 +44,41 @@ struct TriangleShapeView: View {
     }
 }
 
-struct ContentView: View {
+struct ArcShapeView: View {
     var body: some View {
         Arc(startAngle: .degrees(0), endAngle: .degrees(90), clockwise: false)
             .stroke(Color.blue, lineWidth: 10)
             .frame(width: 300, height: 300)
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: clockwise)
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Circle()
+            .strokeBorder(Color.blue, lineWidth: 40)
     }
 }
 
